11dbd6b6bffd6c0f3a9249ccb0cc2a3267dbb273
Aurelie Chauvin <aurelie.chauvin@essec.edu>
2020-11-11 12:03:11 +0100

    rails yelp mvp seed created and run

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian'].freeze
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number ,
+    category: CATEGORY.sample
+  )
+  restaurant.save
+end
